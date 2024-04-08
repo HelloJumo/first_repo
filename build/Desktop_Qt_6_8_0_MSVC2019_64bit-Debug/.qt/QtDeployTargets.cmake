9764611482b38fe22f237b43f77e2a81e9948772
HelloJumo <2315946403@qq.com>
2024-04-08 17:38:18 +0800

    add many files

---
+++ build/Desktop_Qt_6_8_0_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_openposeUI_FILE C:/Users/86186/myProject/QTproject/openposeUI/openposeUI/build/Desktop_Qt_6_8_0_MSVC2019_64bit-Debug/openposeUI.exe)
+set(__QT_DEPLOY_TARGET_openposeUI_RUNTIME_DLLS C:/Qt/6.8.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Cored.dll)
